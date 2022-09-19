c283aa8ff9eb458d2a0ed6a2ff8aeac0f606b174
hemant1847 <65385421+hemant1847@users.noreply.github.com>
2022-09-19 10:24:30 +0530

    Create new_sort.java

---
+++ src/new_sort.java
@@ -0,0 +1,27 @@
+
+public class new_sort {
+	public static void main(String[] args) {
+		// Selecti Sort bubble
+		int arr[] = {2,5,1,3,7,4};
+		int n = arr.length;
+		for (int i = 0; i < arr.length - 1; i++)  
+        {  
+            int index = i;  
+            for (int j = i + 1; j < arr.length; j++){  
+                if (arr[j] < arr[index]){  
+                    index = j;//searching for lowest index  
+                }  
+            }  
+            int smallerNumber = arr[index];   
+            arr[index] = arr[i];  
+            arr[i] = smallerNumber;  
+        }
+		
+		for(int i = 0 ; i < n ; i++) {
+			System.out.println(arr[i]+" ");
+		}
+
+	}
+
+
+}
